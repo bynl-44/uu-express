f61920031e773b0175b0976926e0f8db0059f8a5
gaoshuqing <shuqing.trans@outlook.com>
2024-12-27 15:04:01 +0800

    feat(core): 添加方法参数校验异常处理

--- uu-common/src/main/java/icu/uuqk/common/exception/GlobalExceptionHandler.java
+++ uu-common/src/main/java/icu/uuqk/common/exception/GlobalExceptionHandler.java
@@ -1,14 +1,21 @@
 package icu.uuqk.common.exception;
 
 import icu.uuqk.common.dto.ErrorResponseDto;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
@@ -24,5 +31,22 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
                         .errorTime(LocalDateTime.now())
                         .build()
                 );
+    }
+
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        HashMap<String, String> validationErrors = new HashMap<>();
+        List<ObjectError> allErrors = ex.getBindingResult().getAllErrors();
+
+        for (ObjectError error : allErrors) {
+            String fieldName = ((FieldError) error).getField();
+            String errorMsg = error.getDefaultMessage();
+            validationErrors.put(fieldName, errorMsg);
+        }
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(validationErrors);
     }
 }
