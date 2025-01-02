b3c4924f49c8f8d283c8361096807b1687bf1aca
gaoshuqing <shuqing.gao@qq.com>
2025-01-02 13:39:56 +0800

    feat(core): 添加uu-eureka模块

---
+++ uu-eureka/src/test/java/icu/uuqk/eureka/UuEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package icu.uuqk.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UuEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
